0487b52e2291607577f0fd7ff257cc9b10694332
Aether111 <93689683+Aether111@users.noreply.github.com>
2024-04-26 18:48:54 -0400

    Update app.py
    Still fixing infinite loop

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ if video is not None:
 
         #st.pyplot(graph, use_container_width=True)
 
-    st.download_button("Download Graphs", data=bytes_image, file_name="output.png", mime="image/jpeg", )
+    if st.download_button("Download Graphs", data=bytes_image, file_name="output.png", mime="image/jpeg", ):
 
-    st.experimental_rerun()
+        st.experimental_rerun()
     
     #st.download_button("Download Values", pupil_areas)
