1edaf8aa22201a902de5bb475bf8d7636b5ec4e3
Aether111 <93689683+Aether111@users.noreply.github.com>
2024-04-26 20:40:28 -0400

    Update app.py
    Applying another possible fix for the infinite loop

--- app.py
+++ app.py
@@ -30,30 +30,29 @@ if st.button("Clear uploaded files"):
 
 st.write("Uploaded files:", st.session_state["uploaded_files"])
 
-for i in range(len(files)):
-    video = files[i]
-    if video is not None:
-        g = io.BytesIO(video.read())
-        loc = "current_video.mp4"
-        with open(loc, mode="wb") as f:
-            f.write(g.read())
-    
-        with st.spinner("PROCESSING VIDEO") as s:
-    
-            model = utils.get_model()
-    
-            pupil_areas = utils.process_video(loc, model)
-    
-            graph = utils.plot_pupil_area(pupil_areas)
-    
-            buf = io.BytesIO()
-            img = PIL.Image.open("output.png")
-            img = img.convert("RGB")
-            img.save(buf, format="JPEG")
-            bytes_image = buf.getvalue()
-    
-            #st.pyplot(graph, use_container_width=True)
-    
-        st.download_button("Download Graphs", data=bytes_image, file_name="output.png", mime="image/jpeg", )
-    
-        #st.download_button("Download Values", pupil_areas)
+video = files.pop(0)
+if video is not None:
+    g = io.BytesIO(video.read())
+    loc = "current_video.mp4"
+    with open(loc, mode="wb") as f:
+        f.write(g.read())
+
+    with st.spinner("PROCESSING VIDEO") as s:
+
+        model = utils.get_model()
+
+        pupil_areas = utils.process_video(loc, model)
+
+        graph = utils.plot_pupil_area(pupil_areas)
+
+        buf = io.BytesIO()
+        img = PIL.Image.open("output.png")
+        img = img.convert("RGB")
+        img.save(buf, format="JPEG")
+        bytes_image = buf.getvalue()
+
+        #st.pyplot(graph, use_container_width=True)
+
+    st.download_button("Download Graphs", data=bytes_image, file_name="output.png", mime="image/jpeg", )
+
+    #st.download_button("Download Values", pupil_areas)
