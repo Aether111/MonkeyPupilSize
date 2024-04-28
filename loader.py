886a5d096744d71134d214a3cf4c33211508a3b4
Aether111 <93689683+Aether111@users.noreply.github.com>
2024-04-28 16:34:46 -0400

    Update loader.py
    Fixing set thing

--- loader.py
+++ loader.py
@@ -1,4 +1,8 @@
 def get_new_videos(session_state, current_videos):
-    diff_videos = set(session_state["uploaded_files"]).symmetric_difference(set(current_videos))
+    session_state_names = [video.name for video in session_state["uploaded_files"]]
+    current_video_names = [video.name for video in current_videos]
+    current_video_map = {video.name: video for video in current_videos}
+    diff_videos = set(session_state_names).symmetric_difference(set(current_video_names))
+    diff_videos_output = [current_video_map[name] for name in diff_videos if name in current_video_map]
     session_state["uploaded_files"] = current_videos
-    return list(diff_videos), session_state
+    return diff_videos_output, session_state
