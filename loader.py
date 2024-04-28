1a44249d6792ec4556458782a092e53ddf3123c4
Aether111 <93689683+Aether111@users.noreply.github.com>
2024-04-28 16:19:13 -0400

    Update loader.py
    quick fix

--- loader.py
+++ loader.py
@@ -1,4 +1,4 @@
 def get_new_videos(session_state, current_videos):
     diff_videos = set(session_state["current_videos"]).symmetric_difference(set(current_videos))
-    session_state["current_videos"] = current_videos
+    session_state["uploaded_files"] = current_videos
     return list(diff_videos), session_state
